aede43b8110e8d83d8956a003437b6c43a010b25
totallyrin <lucy.woloszczuk@gmail.com>
2022-11-06 18:59:17 -0500

    added index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = require('express')();
 
 const port = process.env.PORT || 8080;
 
-app.use('/', express.static(path.join('src', 'home.html')));
+app.use(express.static(path.join('src', 'home.html')));
 
 app.listen(port, () => {
     console.log(`Listening on http://localhost:${port}/`);
