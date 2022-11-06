3f589ad1d88ae0458d69c3f54ef507260f43754e
totallyrin <lucy.woloszczuk@gmail.com>
2022-11-06 18:57:11 -0500

    added index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = require('express')();
 
 const port = process.env.PORT || 8080;
 
-app.use('/', express.static(path.join(__dirname, 'testheroku')));
+app.use('/', express.static(path.join('src', 'home.html')));
 
 app.listen(port, () => {
     console.log(`Listening on http://localhost:${port}/`);
